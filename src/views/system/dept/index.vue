b1aa7a119bb2c09458b733a4a8195b49af044c88
zhangbin <zhangqiyi1@163.com>
2024-11-29 14:26:47 +0800

    11

--- src/views/system/dept/index.vue
+++ src/views/system/dept/index.vue
@@ -64,12 +64,12 @@
         </template>
       </el-table-column>
       <el-table-column label="创建时间" align="center" prop="createTime" width="200">
-        <template slot-scope="scope">
+        <template #default="scope">
           <span>{{ parseTime(scope.row.createTime) }}</span>
         </template>
       </el-table-column>
       <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
-        <template slot-scope="scope">
+        <template #default="scope">
           <el-button
             size="mini"
             type="text"
